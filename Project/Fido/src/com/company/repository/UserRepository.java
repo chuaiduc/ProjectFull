eb5e5e003cfaa1da13e4608a3730734a217e5380
chuaiduc <81944254+chuaiduc@users.noreply.github.com>
2021-04-06 22:48:51 +0700

    Delete Project/Fido directory

--- Project/Fido/src/com/company/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.company.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.company.entities.User;
-
-public interface UserRepository extends CrudRepository<User, Integer> {
-	
-}
